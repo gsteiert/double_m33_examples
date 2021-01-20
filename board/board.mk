ec1db9d60e04cc8cb598d428513d320258a48086
Greg Steiert <greg@steiert.net>
2021-01-20 04:30:11 +0000

    switched to NXP mcux-sdk repo

--- board/board.mk
+++ board/board.mk
@@ -13,7 +13,7 @@ CFLAGS += \
 # mcu driver cause following warnings
 CFLAGS += -Wno-error=unused-parameter -Wno-error=float-equal
 
-MCU_DIR = $(TOP)/nxp/sdk/devices/LPC55S69
+MCU_DIR = $(TOP)/mcux-sdk/devices/LPC55S69
 
 # All source paths should be relative to the top level.
 LD_FILE = board/LPC55S69_cm33_core0_uf2.ld
@@ -21,18 +21,23 @@ LD_FILE = board/LPC55S69_cm33_core0_uf2.ld
 SRC_C += \
 	$(MCU_DIR)/system_LPC55S69_cm33_core0.c \
 	$(MCU_DIR)/drivers/fsl_clock.c \
-	$(MCU_DIR)/drivers/fsl_i2c.c \
-	$(MCU_DIR)/drivers/fsl_gpio.c \
+	$(TOP)/mcux-sdk/drivers/flexcomm/fsl_i2c.c \
+	$(TOP)/mcux-sdk/drivers/lpc_gpio/fsl_gpio.c \
 	$(MCU_DIR)/drivers/fsl_power.c \
 	$(MCU_DIR)/drivers/fsl_reset.c \
-	$(MCU_DIR)/drivers/fsl_usart.c \
-	$(MCU_DIR)/drivers/fsl_flexcomm.c \
+	$(TOP)/mcux-sdk/drivers/flexcomm/fsl_usart.c \
+	$(TOP)/mcux-sdk/drivers/flexcomm/fsl_flexcomm.c \
 	$(TOP)/sct_neopixel/sct_neopixel.c
 
 INC += \
 	$(MCU_DIR)/../../CMSIS/Include \
 	$(MCU_DIR) \
 	$(MCU_DIR)/drivers \
+	$(TOP)/mcux-sdk/drivers/common \
+	$(TOP)/mcux-sdk/drivers/sctimer \
+	$(TOP)/mcux-sdk/drivers/flexcomm \
+	$(TOP)/mcux-sdk/drivers/lpc_gpio \
+	$(TOP)/mcux-sdk/drivers/lpc_iocon \
 	$(TOP)/lib \
 	$(TOP)/sct_neopixel
 
